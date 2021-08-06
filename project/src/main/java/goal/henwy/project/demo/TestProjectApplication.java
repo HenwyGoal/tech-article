9e5b76e79b131511ccc6290f586888b7403a4d6a
gaohengwei <gaohengwei@do1.com.cn>
2021-08-06 15:06:24 +0800

    [test-project] test read config from apollo

--- project/src/main/java/goal/henwy/project/demo/TestProjectApplication.java
+++ project/src/main/java/goal/henwy/project/demo/TestProjectApplication.java
@@ -1,15 +1,29 @@
 package goal.henwy.project.demo;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author HenwyGoal
  */
 @EnableDiscoveryClient
+@EnableApolloConfig
 @SpringBootApplication
 public class TestProjectApplication {
+
+	@Value("${test.integer}")
+	private String value;
+
+	@PostConstruct
+	public void init() {
+		System.err.println("test.integer = " + value);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
 	}
